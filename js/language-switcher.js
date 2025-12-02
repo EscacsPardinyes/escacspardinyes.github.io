301e0c27b5f82662a9d746af15cf65a73bda8faf
Jordi Obis Farre <183267726+EscacsPardinyes@users.noreply.github.com>
2025-12-02 21:29:16 +0100

    calculadora

--- js/language-switcher.js
+++ js/language-switcher.js
@@ -25,7 +25,7 @@ function setLanguage(lang) {
     elements.forEach(element => {
         const key = element.getAttribute('data-i18n');
         if (translations[lang] && translations[lang][key]) {
-            element.textContent = translations[lang][key];
+            element.innerHTML = translations[lang][key];
         }
     });
 
